0d4665a11f43afabb63d230079dfd9ae059fb90d
Abraham.Devgit config --global user.email abrahamoluwaniyi50@gmail.com <abrahamoluwaniyi50@gmail.comgit config --global user.name Abraham.Dev>
2026-02-12 17:53:34 +0100

    fix: remove typo in Services component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -48,7 +48,7 @@ export const Services = () => {
           </p>
         </div>
          
-         motio
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {services.map((service, index) => (
             <div 
